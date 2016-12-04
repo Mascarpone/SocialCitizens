aa871c212768b95b6296f667a8a2c12e29840d0a
Florian Le Vern <levern.f@hotmail.fr>
2016-12-04 00:44:30 -0800

    last draft status (not working)

--- src/main/java/io/github/mascarpone/socialcitizens/SocialNPCTrait.java
+++ src/main/java/io/github/mascarpone/socialcitizens/SocialNPCTrait.java
@@ -1,20 +1,17 @@
 package io.github.mascarpone.socialcitizens;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
-import net.citizensnpcs.api.CitizensAPI;
-import net.citizensnpcs.api.ai.Goal;
 import net.citizensnpcs.api.npc.NPC;
-import net.citizensnpcs.api.npc.NPCRegistry;
 import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.api.trait.TraitName;
 
@@ -24,38 +21,62 @@ public class SocialNPCTrait extends Trait {
 	// ********************
 	// PROTOTYPES
 	
-	// Action functions interface: orders to make the NPC act in the Minecraft world
-	protected interface ActionFunction { public void exec(); }
-	
-	// Feedback functions interface: reaction to another SocialNPC's action towards the instance
-	protected interface FeedbackFunction { public void exec(); }
-	
 	// Utility functions interface: evaluation of a need
-	protected interface UtilityFunction { public double exec(); }
+	interface UtilityFunction { public double exec(); }
 	
 	// Solution functions interface: strategy to fulfill a need using action functions
-	protected interface SolutionFunction { public void exec(); }
+	protected interface SolutionFunction { public boolean exec(); }
 	
 	
 	// ********************
 	// VARIABLES
+	
+	// Association between the utility functions and their solutions
+	private Map<UtilityFunction, SolutionFunction> behaviors;
 	
 	// Out-links in the social graph
 	private Map<NPC, SocialLink> social_position;
 	
 	// Personality constants
 	private int default_buddiness;
+	private int hurt_sensibility;
+	private int present_sensibility;
 	private double social_need;
 	private double anti_social_need;
 	private double laziness;
 	
 	
 	// ********************
-	// CONSTRUCTORS
+	// TRAIT INITIALIZATION
 	
 	// Create a SocialNPC using the Citizens2 API and spawn it at the indicated location
 	public SocialNPCTrait() {
 		super("SocialNPC");
+	}
+	
+	//Run code when your trait is attached to a NPC. 
+    //This is called BEFORE onSpawn, so npc.getBukkitEntity() will return null
+    //This would be a good place to load configurable defaults for new NPCs.
+	@Override
+	public void onAttach() {
+		// Initialize the behavior strategies
+		this.behaviors = new HashMap<UtilityFunction, SolutionFunction>();
+		this.behaviors.put(new SatisfactionUtility(), new SatisfactionSolution());
+		
+		// Initialize the graph vision of the NPC
+		this.social_position = new HashMap<NPC, SocialLink>();
+		
+		// Set parameters for an Social Citizen NPC
+		this.npc.setProtected(false);
+				
+		// Set the NPC's personality
+		// TODO: fill the following variables according to the NPC name or randomly for example
+		this.default_buddiness = 1;
+		this.hurt_sensibility = 1;
+		this.present_sensibility = 1;
+		this.social_need = 0;
+		this.anti_social_need = 0;
+		this.laziness = 1;
 	}
 
 	
@@ -78,27 +99,68 @@ public class SocialNPCTrait extends Trait {
 	
 	
 	// ********************
-	// PROTECTED FUNCTIONS
+	// ACTIONS, UTILITIES AND SOLUTIONS
 		
 	// Action: Give a cookie
+	// TODO
+	protected void giveCookie(NPC target, int nb) {
+		this.npc.getNavigator().setTarget(target.getEntity().getLocation().add(1, 0, 1));
+		while(this.npc.getNavigator().isNavigating());
+	}
 	// Action: Hit a player
 	// Action: Be idle
 	
-	// Feedback: Reaction when the instance is being given a cookie
-	// Feedback: Reaction when the instance is being hit
-	
 	// Utility: Evaluate the satisfaction of the instance
+	class SatisfactionUtility implements UtilityFunction { 
+		public double exec() {
+			return laziness;
+		}
+	}
+	
 	// Utility: Evaluate the need of sociability of the instance
+	class SocialNeedUtility implements UtilityFunction { 
+		public double exec() {
+			// TODO implement interesting function HERE
+			return 0;
+		}
+	}
+	
 	// Utility: Evaluate the need of non-sociability of the instance
+	class AnimosityUtility implements UtilityFunction {
+		public double exec() {
+			// TODO implement interesting function HERE
+			return 0;
+		}
+	}
+	
 	
 	// Solution: Strategy to keep a good satisfaction of the current status
 		// --> idle
+	class SatisfactionSolution implements SolutionFunction {
+		public boolean exec() {
+			NPC best_friend = getBestfriend();
+			if (best_friend == null) {
+				if (npc.getEntity() != null) {
+					npc.faceLocation(npc.getEntity().getLocation().add(0, 5, 0));
+					//((Player) npc).playEffect(npc.getEntity().getLocation().add(0, 1, 0), Effect.SMOKE, 2);
+				}
+			}
+			else {
+				npc.getNavigator().setTarget(best_friend.getEntity().getLocation().add(1, 0, 1));
+				npc.faceLocation(best_friend.getEntity().getLocation());
+				while(npc.getNavigator().isNavigating());
+			}
+			return true;
+		}
+	}
 	// Solution: Strategy to lower the instance's need of sociability
 		// --> make a new friend
 		// --> strengthen a friendship
+	// TODO
 	// Solution: Strategy to lower the instance's need of non-sociability
 		// --> make a new enemy
 		// --> strengthen an animosity
+	// TODO
 	
 	
 	// ********************
@@ -115,44 +177,61 @@ public class SocialNPCTrait extends Trait {
 	public void delLink(NPC snpc) {
 		this.social_position.remove(snpc);
 	}
-	
-	// Find the change in the NPC's status and
-	// change the instance's quality of the link with its interlocutor accordingly 
-	public void actionFeedback(NPC interlocutor) {}
-	
+		
 	
 	// ********************	
-	// TRAIT FUNCTIONS
-	
-	//Run code when your trait is attached to a NPC. 
-    //This is called BEFORE onSpawn, so npc.getBukkitEntity() will return null
-    //This would be a good place to load configurable defaults for new NPCs.
-	@Override
-	public void onAttach() {
-		// Initialize the graph vision of the NPC
-		this.social_position = new HashMap<NPC, SocialLink>();
+	// MAIN
 		
-		// Set parameters for an Social Citizen NPC
-		this.npc.setProtected(false);
-				
-		// Set the NPC's personality
-		// TODO: fill the following variables according to the NPC name or randomly for example
-		this.default_buddiness = 0;
-		this.social_need = 0;
-		this.anti_social_need = 0;
-		this.laziness = 0;
-	}
-	
 	// Make a decision based on the utility functions of the class
 	// and apply the corresponding action and feedback
 	// Called every tick
     @Override
-    public void run() {}
+    public void run() {
+    	// Check if the NPC is currently doing an action
+    	// ??? this.npc.getDefaultGoalController().isExecutingGoal()
+    	if (true) {
+    		// Compute the utility functions
+    		Map<Double, SolutionFunction> decision = new HashMap<Double, SolutionFunction>();
+    		for (UtilityFunction uf : this.behaviors.keySet()) {
+    			decision.put(uf.exec(), behaviors.get(uf));
+    		}		
+    		//  Sort the needs and apply the according solution function
+    		for (SolutionFunction sf : new TreeMap<Double, SolutionFunction>(decision).descendingMap().values()) {
+    			if (sf.exec()) break;
+    		}
+    	}
+    }
+    
+    
+	// ********************	
+	// EVENT HANDLERS
 	
     // Change the social link's quality with the attacker 
-    @EventHandler
+    // @EventHandler
     public void NPCDamageByEntityEvent(EntityDamageByEntityEvent event) {
-    	Bukkit.broadcastMessage(this.npc.getName() + " has been damaged.");
+        if(event.getEntity() instanceof NPC) {
+            NPC attacker = (NPC) event.getDamager();
+            if (this.social_position.containsKey(attacker)) {
+            	this.social_position.get(attacker).buddinessDown(this.hurt_sensibility);
+            }
+        }
+        Bukkit.broadcastMessage(this.npc.getName() + "> Ouch!");
+    }
+    
+    // TODO Event Handler for catching a cookie (allow to catch items on the floor)
+    // @EventHandler
+    public void NPCRightClickEvent(Player rightClicker) {
+    	// Check that it has a cookie
+    	// Take the cookie
+    	// Change social links
+    	if (rightClicker instanceof NPC) {
+    		NPC friend = (NPC) rightClicker;
+    		if (this.social_position.containsKey(friend)) {
+            	this.social_position.get(friend).buddinessUp(this.present_sensibility);
+            }
+    	}
+    	// Show particles
+    	Bukkit.broadcastMessage(this.npc.getName() + "> Ty you!");
     }
     
 }
